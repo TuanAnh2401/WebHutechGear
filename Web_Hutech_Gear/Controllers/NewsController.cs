485f070bfd8401d3570f644147e4c75d176c0dd5
NQTuoG <112391190+NQTuoG@users.noreply.github.com>
2023-05-09 22:12:29 +0700

    Add: New Details
    Add new Detail_blog and Detail_News

--- Web_Hutech_Gear/Controllers/NewsController.cs
+++ Web_Hutech_Gear/Controllers/NewsController.cs
@@ -44,5 +44,24 @@ namespace Web_Hutech_Gear.Controllers
 
             return PartialView("Partial_News", pagedList);
         }
+
+        // Chi tiết tin tức
+        public ActionResult DetailNew(int id)
+        {
+            // Tìm kiếm bài viết
+            var news = db.News.SingleOrDefault(x => x.Id == id);
+
+            //Lấy id hiện tại
+            int idCurrent = news.Id;
+
+            //id nhỏ nhất là 1 max là tmp
+            int idMaxTmp = db.News.Count();
+
+            // Truyền dữ liệu sang view bằng Viewbag
+            ViewBag.idCurrent = idCurrent;
+            ViewBag.idMaxTmp = idMaxTmp;
+
+            return View(news);
+        }
     }
 }
